30c00512d05ba1959e36abd9b934453bfabe7569
kkihui <121797755+kkihui@users.noreply.github.com>
2023-07-01 18:14:03 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 884 ms, Memory: 249628 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -4,7 +4,8 @@ N = int(sys.stdin.readline())
 nli = list(map(int,sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 Mli = list(map(int,sys.stdin.readline().split()))
-entli = [0]*20000000 # 2천만개 있으면 -천만 +천만 커버 가능
+
+entli = [0]*20000001# 2천만1개 있으면 -천만 +천만 커버 가능
 resli = [0]*M
 for _ in nli:
     entli[_] += 1
