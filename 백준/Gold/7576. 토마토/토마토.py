6a81c539216c46014f9b23e4f4d76e061fac831f
kkihui <121797755+kkihui@users.noreply.github.com>
2023-10-09 04:07:41 +0900

    [Gold V] Title: 토마토, Time: 3540 ms, Memory: 103224 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,58 @@
+import sys
+import queue
+
+class Graph:
+    def __init__(self,row,col):
+        self.tomato = ([[0]*col for _ in range(row)])
+        self.row,self.col = row,col
+        self.queue = queue.Queue()
+        
+    # (중요) BFS는 큐에서 뺀 다음이 아닌, 큐에 넣을 때 방문 체크를 해야 중복 방문이 일어나지 않음.    
+    def bfs(self): # 비재귀적으로 bfs 구현
+        dr = [0,0,1,-1]
+        dc = [1,-1,0,0]
+        
+        while not self.queue.empty(): # 큐가 빌 때까지 반복
+            now = self.queue.get()
+            
+            for k in range(4): # 4방향 탐색
+                nr = now[0] + dr[k]
+                nc = now[1] + dc[k]
+                if 0<=nr<=self.row-1 and 0<=nc<=self.col-1 and self.tomato[nr][nc] == 0:
+                    self.tomato[nr][nc] = 1
+                    self.queue.put((nr,nc,now[2]+1))
+
+        return now[2] # 마지막으로 처리한 길이가 최대임.
+           
+    def Wheter_all(self): # 토마토 다 익을 수 있는지 없는지 판단 (0인지 아닌지로)
+        for i in range(self.row):
+            for j in range(self.col):
+                if self.tomato[i][j] == 0:
+                    return False
+        return True
+         
+def main():
+    M,N = map(int,sys.stdin.readline().split())
+    g = Graph(N,M)
+    
+    for i in range(N):
+        row_input = list(map(int,sys.stdin.readline().split()))
+        for j in range(M):
+            if row_input[j] == -1: # 빈 곳은 방문했다고 치기
+                g.tomato[i][j] = 1
+            elif row_input[j] == 1:
+                g.tomato[i][j] = 1
+                g.queue.put((i,j,0))
+    
+    if g.queue.empty(): # 익은 사과 없는 경우 예외 처리
+        print(-1)
+    else:
+        day_max = g.bfs()
+        
+        if g.Wheter_all():
+            print(day_max)
+        else:
+            print(-1)
+
+if __name__ == "__main__":
+    main()
