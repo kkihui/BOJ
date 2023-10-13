b0bec82a4d886492387327dabb37d4f02d29658a
kkihui <121797755+kkihui@users.noreply.github.com>
2023-10-13 22:41:43 +0900

    [Silver I] Title: IOIOI, Time: 212 ms, Memory: 33208 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,25 +1,34 @@
 import sys
-
-def set_Pn(num):
-    a = []
-    for i in range(num):
-        if i % 2: # odd
-            a.append("O")
-        else: # even
-            a.append("I")
-    return a
             
 def main():
     N = int(sys.stdin.readline())
     M = int(sys.stdin.readline())
-    S = list(sys.stdin.readline().rstrip())
-    length,cnt = 2*N+1,0
-    Pn = set_Pn(length)
+    S = sys.stdin.readline().rstrip()
+    length,conti,cnt = 2*N+1,0,0
+    
+    if S[0] == 'I':
+        conti = 1
 
-    for _ in range(M-length+1):
-        if S[_:length+_] == Pn:
-            cnt +=1
+    for _ in range(1,M):
+        if conti == 0:
+            if S[_] == 'I':
+                conti = 1
+                
+        else:
+            if S[_] != S[_-1]:
+                conti += 1
+            else:
+                if S[_] == 'I':
+                    conti = 1
+                else:
+                    conti = 0
         
+        if conti >= length:
+            if conti == length:
+                cnt += 1
+            elif (conti-length)%2 == 0:
+                cnt += 1
+                
     print(cnt)
 
 if __name__ == "__main__":
