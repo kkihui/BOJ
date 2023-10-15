9c5e3dc1684ad072799166510b03aee09c104971
kkihui <121797755+kkihui@users.noreply.github.com>
2023-10-15 23:49:25 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int N;
+    
+    scanf("%d",&N);
+
+    if (N == 1)
+    {
+        printf("Leading the Way to the Future\n");
+    }
+    else
+    {
+        printf("YONSEI\n");
+    }
+    
+    return 0;
+}
