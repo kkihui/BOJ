398240b751bfd691aaf305ede589fcdf669948c3
kkihui <121797755+kkihui@users.noreply.github.com>
2023-10-16 17:49:53 +0900

    [Silver I] Title: 절댓값 힙, Time: 128 ms, Memory: 39824 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,23 @@
+import sys
+import heapq
+
+def main():
+    heap = []
+    N = int(sys.stdin.readline())
+    for _ in range(N):
+        num = int(sys.stdin.readline())
+        if num != 0:
+            if num < 0:
+                heapq.heappush(heap,(-num,num))
+            else:
+                heapq.heappush(heap,(num,num))
+                                        
+        else:
+            if heap == []:
+                print(0)
+            else:
+                print(heapq.heappop(heap)[1])
+    
+    
+if __name__ == '__main__':
+    main()
