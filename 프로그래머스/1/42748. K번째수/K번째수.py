f086abdbe108be114b75cd232add13d8fb57a145
kkihui <121797755+kkihui@users.noreply.github.com>
2024-10-11 16:51:37 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,5 @@
 def solution(array, commands):
     answer = []
     for i,j,k in commands:
-        arr = array[i-1:j]
-        arr.sort()
-        num = arr[k-1]
-        answer.append(num)
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
