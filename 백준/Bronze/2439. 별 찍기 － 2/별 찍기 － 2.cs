35734795d89b399a2c4f8babbd097eb6e3eb33b5
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-24 21:26:08 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 356 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < (n - i); j++)
+                    Console.Write(' ');
+                for (int j = 0; j < i; j++)
+                    Console.Write('*');
+                Console.WriteLine();
+            }
+        }
+    }
+}
