7e11cb45cb9698d127ca17b1c490497c8ce7add5
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-26 01:26:07 +0900

    [Bronze I] Title: 비숍 투어, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.cc
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.cc
@@ -10,11 +10,27 @@ int main()
 
 	cin >> n >> m >> sx >> sy >> ex >> ey;
 
-	if ((sx + sy) % 2 == (ex + ey) % 2)
-		ans = "YES";
+	if (n == 1)
+	{
+		if (sy == ey)
+			ans = "YES";
+		else
+			ans = "NO";
+	}
+	else if (m == 1)
+	{
+		if (sx == ex)
+			ans = "YES";
+		else
+			ans = "NO";
+	}
 	else
-		ans = "NO";
-
+	{
+		if ((sx + sy) % 2 == (ex + ey) % 2)
+			ans = "YES";
+		else
+			ans = "NO";
+	}
 	cout << ans << endl;
 
 	return 0;
